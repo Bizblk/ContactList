5ca9cdcf39a7bd1eec3d710abde1247cc269e1f7
Bizblk <enemy_counter@mail.ru>
2021-04-12 22:25:45 +0300

    Initial Commit

---
+++ ContactList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactList
+//
+//  Created by Виталий Оранский on 12.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
